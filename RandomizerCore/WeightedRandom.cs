5b6f42f77fd042273dbb33260170f734638cf9ac
initsu <init.public@gmail.com>
2025-09-26 21:52:54 +0200

    Add another approach to WeightedRandom

--- RandomizerCore/WeightedRandom.cs
+++ RandomizerCore/WeightedRandom.cs
@@ -1,28 +1,78 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
 namespace Z2Randomizer.RandomizerCore;
 
-public class WeightedRandom<T>
+public interface IWeightedSampler<T>
+{
+    T Next(Random r);
+}
+
+
+/// Best option for small weight sums
+/// Construction time proportional to total weight sum.
+/// Memory usage proportional to total weight sum (bad if weights are large).
+/// Sampling is O(1).
+public class TableWeightedRandom<T> : IWeightedSampler<T>
+{
+    private readonly T[] _table;
+    private readonly int _totalWeight;
+
+    public TableWeightedRandom(IEnumerable<(T, int)> entries)
+    {
+        int size;
+        if (entries == null || (size = entries.Count()) == 0) { throw new ArgumentException("Entries cannot be null or empty.", nameof(entries)); }
+
+        List<T> table = new();
+        foreach (var (value, weight) in entries)
+        {
+            for (int i = 0; i < weight; i++)
+            {
+                table.Add(value);
+            }
+        }
+        _table = table.ToArray();
+        _totalWeight = _table.Length;
+    }
+
+    public TableWeightedRandom(IEnumerable<KeyValuePair<T, int>> dict)
+    : this(dict.Select(kvp => (kvp.Key, kvp.Value)))
+    {
+    }
+
+    public T Next([NotNull] Random r)
+    {
+        int roll = r.Next(0, _totalWeight);
+        return _table[roll];
+    }
+}
+
+/// Good for modest n (hundreds or less) where total weight is too big for TableWeightedRandom
+/// Construction time O(n).
+/// Memory usage O(n), independent of weight magnitudes.
+/// Sampling is O(n) (linear scan).
+
+public class LinearWeightedRandom<T> : IWeightedSampler<T>
 {
     private readonly T[] _values;
     private readonly int[] _cumulativeWeights;
     private readonly int _totalWeight;
 
-    public WeightedRandom((T value, int weight)[] entries)
+    public LinearWeightedRandom(IEnumerable<(T, int)> entries)
     {
-        if (entries == null || entries.Length == 0) { throw new ArgumentException("Entries cannot be null or empty.", nameof(entries)); }
+        int size;
+        if (entries == null || (size = entries.Count()) == 0) { throw new ArgumentException("Entries cannot be null or empty.", nameof(entries)); }
 
-        _values = new T[entries.Length];
-        _cumulativeWeights = new int[entries.Length];
+        _values = new T[size];
+        _cumulativeWeights = new int[size];
 
+        int i = 0;
         int total = 0;
-        for (int i = 0; i < entries.Length; i++)
+        foreach (var (value, weight) in entries)
         {
-            T value = entries[i].value;
-            int weight = entries[i].weight;
-
-            if (weight <= 0) { throw new ArgumentException($"Weight must be positive (entry {i})."); }
+            if (weight < 0) { throw new ArgumentException($"Weight must be positive (entry {i})."); }
 
             total += weight;
             _values[i] = value;
@@ -32,11 +82,14 @@ public class WeightedRandom<T>
         _totalWeight = total;
     }
 
-    public T Next(Random rng)
+    public LinearWeightedRandom(IEnumerable<KeyValuePair<T, int>> dict)
+    : this(dict.Select(kvp => (kvp.Key, kvp.Value)))
     {
-        Debug.Assert(rng != null);
+    }
 
-        int roll = rng.Next(0, _totalWeight);
+    public T Next([NotNull] Random r)
+    {
+        int roll = r.Next(0, _totalWeight);
 
         for (int i = 0; i < _cumulativeWeights.Length; ++i)
         {
