7fd4e3a46d6e30d622cb2918502c7ebc242a644a
initsu <init.public@gmail.com>
2026-01-18 23:05:54 +0100

    Create ItemsViewModel

--- CrossPlatformUI/ViewModels/RandomizerViewModel.cs
+++ CrossPlatformUI/ViewModels/RandomizerViewModel.cs
@@ -92,6 +92,7 @@ public class RandomizerViewModel : ReactiveValidationObject, IRoutableViewModel,
         Main = main;
         HostScreen = Main;
         BiomesViewModel = new(Main);
+        ItemsViewModel = new(Main);
         CustomizeViewModel = new(Main);
         Activator = new ViewModelActivator();
 
@@ -314,6 +315,7 @@ public class RandomizerViewModel : ReactiveValidationObject, IRoutableViewModel,
     public MainViewModel Main { get; }
     [JsonIgnore]
     public BiomesViewModel BiomesViewModel { get; }
+    public ItemsViewModel ItemsViewModel { get; }
     public CustomizeViewModel CustomizeViewModel { get; }
     
     [JsonIgnore]
