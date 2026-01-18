69bd75a21eaa410077188b36181b36522f49f413
initsu <init.public@gmail.com>
2026-01-18 23:23:14 +0100

    Add option to remove long dead end palace rooms

--- RandomizerCore/RandomizerConfiguration.cs
+++ RandomizerCore/RandomizerConfiguration.cs
@@ -334,6 +334,11 @@ public sealed partial class RandomizerConfiguration : INotifyPropertyChanged
 
     [Reactive]
     [ConditionallyIncludeInFlags]
+    private bool includeLongDeadEnds = true;
+    public bool includeLongDeadEndsIsIncluded() => palaceStylesAreNotAllVanilla();
+
+    [Reactive]
+    [ConditionallyIncludeInFlags]
     private BossRoomsExitType bossRoomsExitType;
     public bool bossRoomsExitTypeIsIncluded() => palaceStylesAreNotAllVanillaOrShuffled();
 
@@ -1299,7 +1304,6 @@ public sealed partial class RandomizerConfiguration : INotifyPropertyChanged
         }
 
         properties.BlockersAnywhere = blockingRoomsInAnyPalace;
-        properties.IncludeDropRooms = includeDropRooms;
 
         if (bossRoomsExitType == BossRoomsExitType.RANDOM_ALL)
         {
