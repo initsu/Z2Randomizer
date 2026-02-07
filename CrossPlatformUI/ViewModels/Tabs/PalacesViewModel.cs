a2776b7f3e826ad1b696847abed5ad79a82c904b
initsu <init.public@gmail.com>
2026-02-07 10:54:43 +0100

    Add custom random palace style weights

--- CrossPlatformUI/ViewModels/Tabs/PalacesViewModel.cs
+++ CrossPlatformUI/ViewModels/Tabs/PalacesViewModel.cs
@@ -12,10 +12,16 @@ public class PalacesViewModel : ReactiveObject, IActivatableViewModel
     public ViewModelActivator Activator { get; }
     public MainViewModel Main { get; }
 
+    public IObservable<bool> PalaceStyleWeightsIsIncludedObservable { get; }
+
     public PalacesViewModel(MainViewModel main)
     {
         Main = main;
         Activator = new();
+
+        PalaceStyleWeightsIsIncludedObservable = Main.FlagsChanged
+            .Select(_ => Main.Config.palaceStyleWeightsIsIncluded())
+            .DistinctUntilChanged();
 
         this.WhenActivated(OnActivate);
     }
